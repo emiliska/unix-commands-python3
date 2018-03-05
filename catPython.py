cb4c8b585ea7ad9fa729c39af2e8654dd9dafa63
emiliska <33560517+emiliska@users.noreply.github.com>
2018-03-04 22:08:36 -0500

    Update catPython.py

--- catPython.py
+++ catPython.py
@@ -1,7 +1,18 @@
 import sys
 
-for arg in sys.argv[1:]: # loop through arguments from command line
-	with open (arg) as file: # open file
-		contents = file.read() # set contents of file equal to var contents
-		print(contents)
+pretty = "*****"
+
+if len(sys.argv) < 2:
+	print(pretty + 'catPython.py HELP' + pretty
+	+ "\n Hm.. It seems you aren't using this python script quite right." 
+	+ '\n Maybe this example will help:'
+	+ '\n python catPython.py <filename> ...\n'
+	+ pretty + "*****************" + pretty)	
+
+else:
+	for arg in sys.argv[1:]: # loop through arguments from command line
+		with open (arg) as file: # open file
+			# set contents of file equal to var contents
+			contents = file.read() 
+			print(contents)
 		
